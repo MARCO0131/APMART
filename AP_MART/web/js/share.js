e94fceb9cc8f36107fcc2079eb2d51a1e574bd10
MARCO0131 <riteshbadgotya@gmail.com>
2022-04-08 00:09:02 +0530

    store created successfully

---
+++ AP_MART/web/js/share.js
@@ -0,0 +1,24 @@
+/* 
+ 
+ 
+ * and open the template in the editor.
+ */
+
+//$(document).ready(function () {
+//    var Url = location.href;
+//    $("#facebook").attr("href", "https://www.facebook.com/sharer.php?url=" + Url);
+//    $("#twitter").attr("href", "https://twitter.com/share?url=" + Url);
+//    $("#whatsapp").attr("href", "https://api.whatsapp.com/send?text=" + Url);
+//    $("#twitter").attr("href", "https://twitter.com/share?url=" + Url);
+//    $("#googleplus").attr("href", "https://plus.google.com/share?url=" + Url);
+//    $("#pinterest").attr("href", "https://pinterest.com/pin/create/bookmarklet?url=" + Url);
+//    $("#linkedin").attr("href", "https://www.linkedin.com/shareArticle?url=" + Url);    
+//});
+
+$("#share").jsSocials({
+    url: $(this).data('url'),
+    text: "Share Your Friends...",
+    showLabel: false,
+    showCount: false,
+    shares: [ "whatsapp", "email", "facebook", "twitter",'googleplus', "pinterest"]
+});
